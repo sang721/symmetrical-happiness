051ce2d9022ff29a8633f2b3adfdb24c50827269
Nguyen_Dinh_Sang <nazisang33@gmail.com>
2020-12-01 14:41:57 +0700

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.

---
+++ crawler_2.py
@@ -0,0 +1,16 @@
+import bs4
+from bs4 import BeautifulSoup as bs
+import selenium
+from selenium import webdriver
+
+def get_page(url):
+    driver = webdriver.Chrome(executable_path= "C:\Program Files\Google\chromedriver.exe")
+    driver.set_window_position(-10000,0)
+    driver.get(url)
+    body = driver.page_source
+    driver.close()
+    soup = bs(body,"html.parser")
+    return soup.prettify()
+urls =[ "https://Zing.vn","https://24h.vn","https://dantri.vn","https://nhandan.vn","https://thanhnien.vn"]
+for url in urls :
+    get_page(url)
