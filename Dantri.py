599ba5b5e4f6ea21eb9fa2703fa311bcedcfd9f8
Nguyen_Dinh_Sang <nazisang33@gmail.com>
2020-12-01 21:28:07 +0700

    Ok

--- Dantri.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-class Bao_Dantri(scrapy.Spider):
-    name = "Dan tri"
-    def start_requests(self):
-        start_urls = "https://nhandan.com.vn/"
-        return scrapy.Request(url = start_urls ,callback= self.parse,)
-    def parse(self,response):
-        pass
